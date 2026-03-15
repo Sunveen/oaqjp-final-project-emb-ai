61cfe5586b1c305bf3b5f156294e85147eabed09
Sunveen <sunveen07@gmail.com>
2026-03-15 12:02:31 -0400

    Added emotion_detection fn

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    data = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = data, headers = headers)
+    format_data = json.loads(response.text)
+    anger_score = format_data['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = format_data['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = format_data['emotionPredictions'][0]['emotion']['fear']
+    joy_score = format_data['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = format_data['emotionPredictions'][0]['emotion']['sadness']
+    emotions = format_data['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+    return {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            'dominant_emotion': dominant_emotion
+            }
