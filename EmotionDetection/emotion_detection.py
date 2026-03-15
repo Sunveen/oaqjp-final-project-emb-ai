c1f6db2c587ebfea4d6857bac8fc671117f59800
Sunveen <sunveen07@gmail.com>
2026-03-15 14:44:34 -0400

    fixed lint errors

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,6 +6,15 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     data = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = data, headers = headers)
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+            }
     format_data = json.loads(response.text)
     anger_score = format_data['emotionPredictions'][0]['emotion']['anger']
     disgust_score = format_data['emotionPredictions'][0]['emotion']['disgust']
--- server.py
+++ server.py
@@ -1,24 +1,40 @@
+"""
+Application to detect emotion from the provided text
+"""
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
-
 
 app = Flask(__name__)
 
 @app.route("/emotionDetector")
 def emotion_detect():
+    """
+    Route to return emotion using Watson AI
+    """
     text = request.args.get('textToAnalyze')
     response = emotion_detector(text)
+
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!."
+
     dominant_emotion = response["dominant_emotion"]
     anger = response["anger"]
     disgust = response["disgust"]
     fear = response["fear"]
     joy = response["joy"]
     sadness = response["sadness"]
-    return f"For the given statement, the system response is 'anger':{anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    return (
+    f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, "
+    f"'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
+    f"The dominant emotion is {dominant_emotion}."
+)
 
 @app.route("/")
-def loadIndex():
+def load_index():
+    """
+    Route to return index file
+    """
     return render_template('index.html')
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
